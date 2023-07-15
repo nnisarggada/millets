77b938c66cface70d298ca696a12d7acd6e5fe8b
nnisarggada <gadannisarg@gmail.com>
2023-07-15 18:28:36 +0530

    site done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,33 +12,36 @@ export default function Hero() {
           <br />
           (Wait-A-Millet)
         </h1>
-        <p>
-          An entertaining and informative play about the importance of millet
-          for India
-        </p>
-        <h1 className="text-xl font-bold">Synopsis</h1>
-        <p>
-          A musical play narrated by a Kirtankaar about mother nature and soil,
-          through a metaphorical format where cotton and millet as characters
-          take the audience to a musical ride of emotions,conflict and a journey
-          reflecting on every individual's role as Farmers of the Earth!
-        </p>
-        <button
-          className="w-64 h-16 self-center bg-secondary text-primary font-bold rounded-xl transition-all hover:drop-shadow-2xl hover:scale-110"
-          onClick={() => {
-            window.location.href = "/shows";
-          }}
-        >
-          Organise Shows
-        </button>
-        <button
-          className="w-64 h-16 self-center bg-secondary text-primary font-bold rounded-xl transition-all hover:drop-shadow-2xl hover:scale-110"
-          onClick={() => {
-            window.location.href = "#";
-          }}
-        >
-          Book Tickets
-        </button>
+        <div className="h-full flex flex-col justify-between">
+          <p>
+            An entertaining and informative play about the importance of millet
+            for India
+          </p>
+          <h1 className="text-xl font-bold">Synopsis</h1>
+          <p>
+            A musical play narrated by a Kirtankaar about mother nature and
+            soil, through a metaphorical format where cotton and millet as
+            characters take the audience to a musical ride of emotions,conflict
+            and a journey reflecting on every individual's role as Farmers of
+            the Earth!
+          </p>
+          <button
+            className="w-64 h-16 self-center bg-secondary text-primary font-bold rounded-xl transition-all hover:drop-shadow-2xl hover:scale-110"
+            onClick={() => {
+              window.location.href = "/shows";
+            }}
+          >
+            Organise Shows
+          </button>
+          <button
+            className="w-64 h-16 self-center bg-secondary text-primary font-bold rounded-xl transition-all hover:drop-shadow-2xl hover:scale-110"
+            onClick={() => {
+              window.location.href = "#";
+            }}
+          >
+            Book Tickets
+          </button>
+        </div>
       </section>
     </>
   );
